439530fc07bcae6336f91c4ad3eef0dbedf06782
Bryce Haley <bhaley91@gmail.com>
2022-11-05 15:28:47 -0400

    images added seeding starts

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO users(email, password, name)
+VALUES
+('a@google.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','user a'),
+('b@google.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','user b'),
+('c@google.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.','user c');
+
+INSERT INTO properties(
+  description, 
+  cost_per_night,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms,
+  thumbnail_url,
+  cover_photo_url,
+  status,
+  country,
+  street,
+  provice,
+  post_code,
+  owner_id)
+VALUES
+('lorem ipsum', 10.25, 0, 15, )
